84d45403ad42eb68dc162efef7e5d471c4712fa3
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-12-05 17:38:10 +0700

    feat: add base form request for custom validation errors

---
+++ app/Http/Requests/BaseFormRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class BaseFormRequest extends FormRequest
+{
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new ValidationException($validator, response()->json([
+            'status' => 'fail',
+            'data' => $validator->errors(),
+        ], 422));
+    }
+}
