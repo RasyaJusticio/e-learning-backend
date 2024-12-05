7024b62b8eb921793cb1c71997c4cf51669be035
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-12-05 17:22:21 +0700

    feat: implement user registration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    /**
+     * URL => /api/auth/register
+     * METHOD => POST
+     */
+    public function register(RegisterRequest $request)
+    {
+        $fields = $request->validated();
+
+        // Creates a unique name from their email
+        $namePart = strstr($fields['email'], '@', true);
+        $uniqueId = Str::random(6);
+
+        User::create([
+            'name' => ucfirst($namePart) . $uniqueId,
+            'username' => $namePart . $uniqueId,
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => null
+        ], 200);
+    }
+}
