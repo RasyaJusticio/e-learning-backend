a7ee32e2bdcd5d28eaa13b1fc1bb58f217751ebb
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-12-08 22:42:15 +0700

    feat: handle not found route exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,8 @@ use App\Http\Middleware\TeacherOnly;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,5 +23,17 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                $message = $e->getMessage();
+
+                if (str_contains($message, 'The route')) {
+                    return response()->json([
+                        'status' => 'fail',
+                        'message' => 'Route not found',
+                        'data' => null
+                    ], 404);
+                }
+            }
+        });
     })->create();
