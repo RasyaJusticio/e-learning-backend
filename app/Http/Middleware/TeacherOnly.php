f74418be05b3f60199716005f1b118b750df62e7
RasyaJusticio <rasyajusticio.personal@gmail.com>
2024-12-08 19:04:34 +0700

    feat: create teacher only middleware

---
+++ app/Http/Middleware/TeacherOnly.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TeacherOnly
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!isset($request->user)) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Unauthorized',
+                'data' => null
+            ], 401);
+        }
+
+        if ($request->user()->role !== 'teacher') {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Forbidden',
+                'data' => null
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
